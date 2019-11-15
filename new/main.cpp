d19ea602891ec7d9f05a93ac75b8594ae74fedc6
Rahul B S <bsrahul2020@gmail.com>
2019-11-16 03:04:55 +0530

    modified

--- new/main.cpp
+++ new/main.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
+#include <time.h>
 #include <sys/stat.h>
 #include <unistd.h>
 #include <vector>
@@ -24,7 +25,7 @@ Global Declarations
 #define GAME_ON 1
 #define GAME_WON 2
 
-#define MAZE_SIZE 13
+#define MAZE_SIZE 21
 
 // Main Window
 int mainWindow;
@@ -209,21 +210,199 @@ void draw_text(const char* text)
 }
 
 //Array for map layout. 0 for wall cubes and 2 for ground
-int maze[MAZE_SIZE][MAZE_SIZE] = {
-	   {2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0} ,
-	   {0, 2, 0, 2, 0, 0, 0, 2, 2, 2, 2, 0, 0} ,
-	   {0, 2, 2, 2, 2, 0, 0, 2, 0, 2, 2, 2, 2} ,
-	   {0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 2} ,
-	   {0, 0, 2, 2, 0, 2, 0, 0, 0, 2, 0, 0, 2},
-	   {0, 0, 0, 2, 0, 0, 0, 2, 0, 2, 0, 2, 2} ,
-	   {0, 0, 0, 2, 0, 0, 0, 2, 0, 2, 0, 0, 0} ,
-	   {0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0, 2, 2} ,
-	   {0, 2, 2, 2, 0, 2, 0, 2, 0, 2, 0, 0, 0} ,
-	   {0, 0, 2, 2, 0, 0, 2, 2, 2, 2, 2, 0, 0},
-	   {0, 2, 2, 2, 0, 0, 2, 0, 0, 2, 2, 2, 0},
-	   {0, 0, 2, 0, 0, 0, 2, 0, 2, 2, 2, 2, 0},
-		 {0, 2, 2, 2, 0, 0, 2, 2, 0, 0, 0, 0, 0}
-};
+// int maze[MAZE_SIZE][MAZE_SIZE] = {
+// 	   {2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0} ,
+// 	   {0, 2, 0, 2, 0, 0, 0, 2, 2, 2, 2, 0, 0} ,
+// 	   {0, 2, 2, 2, 2, 0, 0, 2, 0, 2, 2, 2, 2} ,
+// 	   {0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 2} ,
+// 	   {0, 0, 2, 2, 0, 2, 0, 0, 0, 2, 0, 0, 2},
+// 	   {0, 0, 0, 2, 0, 0, 0, 2, 0, 2, 0, 2, 2} ,
+// 	   {0, 0, 0, 2, 0, 0, 0, 2, 0, 2, 0, 0, 0} ,
+// 	   {0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0, 2, 2} ,
+// 	   {0, 2, 2, 2, 0, 2, 0, 2, 0, 2, 0, 0, 0} ,
+// 	   {0, 0, 2, 2, 0, 0, 2, 2, 2, 2, 2, 0, 0},
+// 	   {0, 2, 2, 2, 0, 0, 2, 0, 0, 2, 2, 2, 0},
+// 	   {0, 0, 2, 0, 0, 0, 2, 0, 2, 2, 2, 2, 0},
+// 		 {0, 2, 2, 2, 0, 0, 2, 2, 0, 0, 0, 0, 0}
+// };
+
+typedef struct
+{
+	int x, y; //Node position - little waste of memory, but it allows faster generation
+	void *parent; //Pointer to parent node
+	char c; //Character to be displayed
+	char dirs; //Directions that still haven't been explored
+} Node;
+
+Node *nodes; //Nodes array
+const int width=MAZE_SIZE, height=MAZE_SIZE; //Maze dimensions
+int maze[width][height];
+
+int init_maze( )
+{
+	int i, j;
+	Node *n;
+	
+	//Allocate memory for maze
+	nodes = (Node*)calloc( width * height, sizeof( Node ) );
+	if ( nodes == NULL ) return 1;
+		
+	//Setup crucial nodes
+	for ( i = 0; i < width; i++ )
+	{
+		for ( j = 0; j < height; j++ )
+		{
+			n = nodes + i + j * width;
+			if ( i * j % 2 ) 
+			{
+				n->x = i;
+				n->y = j;
+				n->dirs = 15; //Assume that all directions can be explored (4 youngest bits set)
+				n->c = ' '; 
+			}
+			else n->c = '#'; //Add walls between nodes
+		}
+	}
+	return 0;
+}
+
+Node *link_node( Node *n )
+{
+	//Connects node to random neighbor (if possible) and returns
+	//address of next node that should be visited
+
+	int x, y;
+	char dir;
+	Node *dest;
+	
+	//Nothing can be done if null pointer is given - return
+	if ( n == NULL ) return NULL;
+	
+	//While there are directions still unexplored
+	while ( n->dirs )
+	{
+		//Randomly pick one direction
+		dir = ( 1 << ( rand( ) % 4 ) );
+		
+		//If it has already been explored - try again
+		if ( ~n->dirs & dir ) continue;
+		
+		//Mark direction as explored
+		n->dirs &= ~dir;
+		
+		//Depending on chosen direction
+		switch ( dir )
+		{
+			//Check if it's possible to go right
+			case 1:
+				if ( n->x + 2 < width )
+				{
+					x = n->x + 2;
+					y = n->y;
+				}
+				else continue;
+				break;
+			
+			//Check if it's possible to go down
+			case 2:
+				if ( n->y + 2 < height )
+				{
+					x = n->x;
+					y = n->y + 2;
+				}
+				else continue;
+				break;
+			
+			//Check if it's possible to go left	
+			case 4:
+				if ( n->x - 2 >= 0 )
+				{
+					x = n->x - 2;
+					y = n->y;
+				}
+				else continue;
+				break;
+			
+			//Check if it's possible to go up
+			case 8:
+				if ( n->y - 2 >= 0 )
+				{
+					x = n->x;
+					y = n->y - 2;
+				}
+				else continue;
+				break;
+		}
+		
+		//Get destination node into pointer (makes things a tiny bit faster)
+		dest = nodes + x + y * width;
+		
+		//Make sure that destination node is not a wall
+		if ( dest->c == ' ' )
+		{
+			//If destination is a linked node already - abort
+			if ( dest->parent != NULL ) continue;
+			
+			//Otherwise, adopt node
+			dest->parent = n;
+			
+			//Remove wall between nodes
+			nodes[n->x + ( x - n->x ) / 2 + ( n->y + ( y - n->y ) / 2 ) * width].c = ' ';
+			
+			//Return address of the child node
+			return dest;
+		}
+	}
+	
+	//If nothing more can be done here - return parent's address
+	return (Node*)(n->parent);
+}
+
+void draw( )
+{
+	int i, j;
+
+	//Outputs maze to terminal - nothing special
+	for ( i = 0; i < height; i++ )
+	{
+		for ( j = 0; j < width; j++ )
+		{
+			cout<<nodes[j + i * width].c;
+      if(nodes[j + i * width].c == '#'){
+        maze[i][j] = 0;
+      }
+      else{
+        maze[i][j] = 2;
+      }
+		}
+		cout<<"\n";
+	}
+}
+
+
+void mazeGen(){
+	Node *start, *last;
+
+	//Seed random generator
+	srand( time( NULL ) );
+	
+	//Initialize maze
+	if ( init_maze( ) )
+	{
+		fprintf( stderr, "out of memory for maze!\n" );
+		exit( 1 );
+	}
+	
+	//Setup start node
+	start = nodes + 1 + width;
+	start->parent = start;
+	last = start;
+	
+	//Connect nodes until start node is reached and can't be left
+	while ( ( last = link_node( last ) ) != start );
+	draw( );
+
+}
 
 //Function for window resize
 void reshape(int w, int h)
@@ -675,6 +854,7 @@ void init() {
 	glEnable(GL_DEPTH_TEST);
 	glShadeModel(GL_SMOOTH);
 	set_material (materialM);
+	mazeGen();
 }
 
 int main(int argc, char **argv) {
