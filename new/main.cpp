aafe0b388480de18071cd8fbb12341fbf309e695
divyachn <dvchhn7@gmail.com>
2019-11-16 14:45:59 +0530

    Added Welcome Screen

--- new/main.cpp
+++ new/main.cpp
@@ -16,16 +16,13 @@
 
 using namespace std;
 
-/*
-Global Declarations
-*/
-
-
+#define MAZE_SIZE 13
 #define GAME_START 0
 #define GAME_ON 1
 #define GAME_WON 2
 
-#define MAZE_SIZE 13
+// Game state
+int gameState = 0;
 
 // Main Window
 int mainWindow;
@@ -42,9 +39,6 @@ float cameraMoveSpeed = 0.1f;
 bool mapMode = false;
 bool win = false;
 
-// Game state
-int gameState = 1;
-
 //Angle of rotation, direction vector of camera and position vector of camera
 float angle=0.0f;
 float lx=0.0f,lz=-1.0f, ly=0;
@@ -53,8 +47,6 @@ float x=2 ,z=2, y = 0;
 
 float origTilt;
 float origLy;
-
-
 
 //Angle for blip
 float turnAngle = 0;
@@ -65,7 +57,6 @@ float deltaAngleY = 0.0f;
 float deltaX = 0;
 float deltaZ = 0;
 
-
 unsigned int g_wall = 0;
 unsigned int g_ground = 1;
 unsigned int g_start = 2;
@@ -73,16 +64,14 @@ unsigned int g_start = 2;
 unsigned int g_bitmap_text_handle = 0;
 
 //Datastructures for lights and material properties
-struct materials_t
-{
+struct materials_t {
 	float ambient[4];
 	float diffuse[4];
 	float specular[4];
 	float shininess;
 };
 
-struct light_t
-{
+struct light_t {
 	int id;
 	size_t name;
 	float ambient[4];
@@ -114,13 +103,8 @@ light_t light_2 = {
 	{0.5f, 0.5f, 0.5f, 1.0f}
 };
 
-/****************************
-FUNCTIONS
-****************************/
-
 //Properties of a given material
-void set_material(const materials_t& mat)
-{
+void set_material(const materials_t& mat) {
 	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, mat.ambient);
 	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, mat.diffuse);
 	glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, mat.specular);
@@ -128,8 +112,7 @@ void set_material(const materials_t& mat)
 }
 
 //Set a given light
-void set_light(const light_t& light, float lightx, float lightz)
-{
+void set_light(const light_t& light, float lightx, float lightz) {
 	float lighty;
 	float intensity;
 	float dir;
@@ -154,27 +137,21 @@ void set_light(const light_t& light, float lightx, float lightz)
 	//Spotlight adjustment calls
 	float direction[3] = {0, dir, 0};
 	glLightfv(light.name, GL_SPOT_DIRECTION, direction);
-
 	glLightf(light.name, GL_SPOT_CUTOFF, intensity);
-
-
 	glEnable(light.name);
 }
 
 //Function for handling menu events
-void processMenuEvents(int option)
-{
+void processMenuEvents(int option) {
 	//Exit cleanly
 	if (option == 2) {
 		glutDestroyWindow(mainWindow);
 		exit(0);
 	}
-
 }
 
 //Menu creator function for mouse RB clicks
-void createGLUTMenus()
-{
+void createGLUTMenus() {
 	int menu;
 
 	//"processMenuEvents" will handle events
@@ -189,12 +166,9 @@ void createGLUTMenus()
 }
 
 //Bitmap text function
-unsigned int make_bitmap_text()
-{
+unsigned int make_bitmap_text() {
 	unsigned int handle_base = glGenLists(256);
-
-	for (size_t i=0;i<256;i++)
-	{
+	for (size_t i=0;i<256;i++) {
 		// a new list for each character
 		glNewList(handle_base+i, GL_COMPILE);
 			glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, i);
@@ -203,31 +177,13 @@ unsigned int make_bitmap_text()
 	return handle_base;
 }
 
-void draw_text(const char* text)
-{
+void draw_text(const char* text) {
 	glListBase(g_bitmap_text_handle);
 	glCallLists(int(strlen(text)), GL_UNSIGNED_BYTE, text);
 }
 
 //Array for map layout. 0 for wall cubes and 2 for ground
-// int maze[MAZE_SIZE][MAZE_SIZE] = {
-// 	   {2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0} ,
-// 	   {0, 2, 0, 2, 0, 0, 0, 2, 2, 2, 2, 0, 0} ,
-// 	   {0, 2, 2, 2, 2, 0, 0, 2, 0, 2, 2, 2, 2} ,
-// 	   {0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 2} ,
-// 	   {0, 0, 2, 2, 0, 2, 0, 0, 0, 2, 0, 0, 2},
-// 	   {0, 0, 0, 2, 0, 0, 0, 2, 0, 2, 0, 2, 2} ,
-// 	   {0, 0, 0, 2, 0, 0, 0, 2, 0, 2, 0, 0, 0} ,
-// 	   {0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0, 2, 2} ,
-// 	   {0, 2, 2, 2, 0, 2, 0, 2, 0, 2, 0, 0, 0} ,
-// 	   {0, 0, 2, 2, 0, 0, 2, 2, 2, 2, 2, 0, 0},
-// 	   {0, 2, 2, 2, 0, 0, 2, 0, 0, 2, 2, 2, 0},
-// 	   {0, 0, 2, 0, 0, 0, 2, 0, 2, 2, 2, 2, 0},
-// 		 {0, 2, 2, 2, 0, 0, 2, 2, 0, 0, 0, 0, 0}
-// };
-
-typedef struct
-{
+typedef struct {
 	int x, y; //Node position - little waste of memory, but it allows faster generation
 	void *parent; //Pointer to parent node
 	char c; //Character to be displayed
@@ -239,30 +195,26 @@ const int width=MAZE_SIZE, height=MAZE_SIZE; //Maze dimensions
 int maze[width][height];
 int diamondx, diamondz;
 
-int init_maze( )
-{
+int init_maze( ) {
 	int i, j;
 	Node *n;
 
 	diamondx = 2*(1 + 2*(rand()%(width/2))); // *2 for world coordinate
 	diamondz = 2*(1 + 2*(rand()%(height/2)));
-	
+
 	//Allocate memory for maze
 	nodes = (Node*)calloc( width * height, sizeof( Node ) );
 	if ( nodes == NULL ) return 1;
-		
+
 	//Setup crucial nodes
-	for ( i = 0; i < width; i++ )
-	{
-		for ( j = 0; j < height; j++ )
-		{
+	for ( i = 0; i < width; i++ ) {
+		for ( j = 0; j < height; j++ ) {
 			n = nodes + i + j * width;
-			if ( i * j % 2 ) 
-			{
+			if ( i * j % 2 ) {
 				n->x = i;
 				n->y = j;
 				n->dirs = 15; //Assume that all directions can be explored (4 youngest bits set)
-				n->c = ' '; 
+				n->c = ' ';
 			}
 			else n->c = '#'; //Add walls between nodes
 		}
@@ -270,112 +222,98 @@ int init_maze( )
 	return 0;
 }
 
-Node *link_node( Node *n )
-{
+Node *link_node( Node *n ) {
 	//Connects node to random neighbor (if possible) and returns
 	//address of next node that should be visited
-
 	int x, y;
 	char dir;
 	Node *dest;
-	
+
 	//Nothing can be done if null pointer is given - return
 	if ( n == NULL ) return NULL;
-	
+
 	//While there are directions still unexplored
-	while ( n->dirs )
-	{
+	while ( n->dirs ) {
 		//Randomly pick one direction
 		dir = ( 1 << ( rand( ) % 4 ) );
-		
+
 		//If it has already been explored - try again
 		if ( ~n->dirs & dir ) continue;
-		
+
 		//Mark direction as explored
 		n->dirs &= ~dir;
-		
+
 		//Depending on chosen direction
-		switch ( dir )
-		{
+		switch ( dir ) {
 			//Check if it's possible to go right
 			case 1:
-				if ( n->x + 2 < width )
-				{
+				if ( n->x + 2 < width ) {
 					x = n->x + 2;
 					y = n->y;
 				}
 				else continue;
 				break;
-			
+
 			//Check if it's possible to go down
 			case 2:
-				if ( n->y + 2 < height )
-				{
+				if ( n->y + 2 < height ) {
 					x = n->x;
 					y = n->y + 2;
 				}
 				else continue;
 				break;
-			
-			//Check if it's possible to go left	
+
+			//Check if it's possible to go left
 			case 4:
-				if ( n->x - 2 >= 0 )
-				{
+				if ( n->x - 2 >= 0 ) {
 					x = n->x - 2;
 					y = n->y;
 				}
 				else continue;
 				break;
-			
+
 			//Check if it's possible to go up
 			case 8:
-				if ( n->y - 2 >= 0 )
-				{
+				if ( n->y - 2 >= 0 ) {
 					x = n->x;
 					y = n->y - 2;
 				}
 				else continue;
 				break;
 		}
-		
+
 		//Get destination node into pointer (makes things a tiny bit faster)
 		dest = nodes + x + y * width;
-		
+
 		//Make sure that destination node is not a wall
-		if ( dest->c == ' ' )
-		{
+		if ( dest->c == ' ' ) {
 			//If destination is a linked node already - abort
 			if ( dest->parent != NULL ) continue;
-			
+
 			//Otherwise, adopt node
 			dest->parent = n;
-			
+
 			//Remove wall between nodes
 			nodes[n->x + ( x - n->x ) / 2 + ( n->y + ( y - n->y ) / 2 ) * width].c = ' ';
-			
+
 			//Return address of the child node
 			return dest;
 		}
 	}
-	
+
 	//If nothing more can be done here - return parent's address
 	return (Node*)(n->parent);
 }
 
-void draw( )
-{
-	int i, j;
-
+void draw( ) {
 	//Outputs maze to terminal - nothing special
-	for ( i = 0; i < height; i++ )
-	{
-		for ( j = 0; j < width; j++ )
-		{
+	for ( int i = 0; i < height; i++ ) {
+		for ( int j = 0; j < width; j++ ) {
 			cout<<nodes[j + i * width].c;
-      if(nodes[j + i * width].c == '#'){
+      if(nodes[j + i * width].c == '#') {
         maze[i][j] = 0;
       }
-      else{
+      else {
         maze[i][j] = 2;
       }
 		}
@@ -383,132 +321,106 @@ void draw( )
 	}
 }
 
-
-void mazeGen(){
+void mazeGen() {
 	Node *start, *last;
-
 	//Seed random generator
 	srand( time( NULL ) );
-	
+
 	//Initialize maze
-	if ( init_maze( ) )
-	{
+	if ( init_maze( ) ) {
 		fprintf( stderr, "out of memory for maze!\n" );
 		exit( 1 );
 	}
-	
+
 	//Setup start node
 	start = nodes + 1 + width;
 	start->parent = start;
 	last = start;
-	
 	//Connect nodes until start node is reached and can't be left
 	while ( ( last = link_node( last ) ) != start );
-	draw( );
-
+	draw();
 }
 
 //Function for window resize
-void reshape(int w, int h)
-{
+void reshape(int w, int h) {
 	SizeX=w;
 	SizeY=h;
-
 	xOrigin = SizeX/2;
 	yOrigin = SizeY/2;
-
-	//Prevent divide by 0
-	if (h == 0)
+	if (h == 0) //Prevent divide by 0
 		h = 1;
 	float ratio =  w * 1.0 / h;
-
-	// Use the Projection Matrix
 	glMatrixMode(GL_PROJECTION);
-
-	// Reset Matrix
 	glLoadIdentity();
-
-	// Set the viewport to be the entire window
 	glViewport(0, 0, w, h);
-
-	// Set the correct perspective.
 	gluPerspective(45.0f, ratio, 0.1f, 100.0f);
 }
 
-//Function to draw wall cube
-void drawWall(float x1, float x2, float y1, float y2, float z1, float z2){
+void drawWall(float x1, float x2, float y1, float y2, float z1, float z2) {
 	glBindTexture ( GL_TEXTURE_2D, g_wall);
 	glBegin(GL_QUADS);
-	glColor4f(1, 1, 1, 1);
+		glColor4f(1, 1, 1, 1);
+		//Rightside
+		glNormal3f(0, 0, 1);
+		glTexCoord2f(0,0);
+		glVertex3f(x1, y1, z1);
+	  glTexCoord2f(1,0);
+		glVertex3f(x2, y1, z1);
+		glTexCoord2f(1,1);
+		glVertex3f(x2, y2, z1);
+		glTexCoord2f(0,1);
+		glVertex3f(x1, y2, z1);
 
-	//Rightside
-	glNormal3f(0, 0, 1);
-	glTexCoord2f(0,0);
-	glVertex3f(x1, y1, z1);
-        glTexCoord2f(1,0);
-	glVertex3f(x2, y1, z1);
-	glTexCoord2f(1,1);
-	glVertex3f(x2, y2, z1);
-	glTexCoord2f(0,1);
-	glVertex3f(x1, y2, z1);
+		//Leftside
+		glNormal3f(0, 0, -1);
+		glTexCoord2f(0,0);
+		glVertex3f(x1, y1, z2);
+		glTexCoord2f(1,0);
+		glVertex3f(x2, y1, z2);
+		glTexCoord2f(1,1);
+		glVertex3f(x2, y2, z2);
+		glTexCoord2f(0,1);
+		glVertex3f(x1, y2, z2);
 
+		//Top
+		glNormal3f (0,1, 0);
+		glTexCoord2f(0,0);
+		glVertex3f(x1, y2, z1);
+		glTexCoord2f(1,0);
+		glVertex3f(x2, y2, z1);
+		glTexCoord2f(1,1);
+		glVertex3f(x2, y2, z2);
+		glTexCoord2f(0,1);
+		glVertex3f(x1, y2, z2);
 
-	//Leftside
-	glNormal3f(0, 0, -1);
-	glTexCoord2f(0,0);
-	glVertex3f(x1, y1, z2);
-	glTexCoord2f(1,0);
-	glVertex3f(x2, y1, z2);
-	glTexCoord2f(1,1);
-	glVertex3f(x2, y2, z2);
-	glTexCoord2f(0,1);
-	glVertex3f(x1, y2, z2);
+		//Backside
+		glNormal3f (1,0, 0);
+		glTexCoord2f(0,0);
+		glVertex3f(x1, y1, z1);
+		glTexCoord2f(0,1);
+		glVertex3f(x1, y2, z1);
+		glTexCoord2f(1,1);
+		glVertex3f(x1, y2, z2);
+		glTexCoord2f(1,0);
+		glVertex3f(x1, y1, z2);
 
-
-	//Top
-	glNormal3f (0,1, 0);
-	glTexCoord2f(0,0);
-	glVertex3f(x1, y2, z1);
-	glTexCoord2f(1,0);
-	glVertex3f(x2, y2, z1);
-	glTexCoord2f(1,1);
-	glVertex3f(x2, y2, z2);
-	glTexCoord2f(0,1);
-	glVertex3f(x1, y2, z2);
-
-	//Backside
-	glNormal3f (1,0, 0);
-	glTexCoord2f(0,0);
-	glVertex3f(x1, y1, z1);
-	glTexCoord2f(0,1);
-	glVertex3f(x1, y2, z1);
-	glTexCoord2f(1,1);
-	glVertex3f(x1, y2, z2);
-	glTexCoord2f(1,0);
-	glVertex3f(x1, y1, z2);
-
-
-	//Frontside
-	glNormal3f (-1,0, 0);
-	glTexCoord2f(0,0);
-	glVertex3f(x2, y1, z1);
-	glTexCoord2f(0,1);
-	glVertex3f(x2, y2, z1);
-	glTexCoord2f(1,1);
-	glVertex3f(x2, y2, z2);
-	glTexCoord2f(1,0);
-	glVertex3f(x2, y1, z2);
-
+		//Frontside
+		glNormal3f (-1,0, 0);
+		glTexCoord2f(0,0);
+		glVertex3f(x2, y1, z1);
+		glTexCoord2f(0,1);
+		glVertex3f(x2, y2, z1);
+		glTexCoord2f(1,1);
+		glVertex3f(x2, y2, z2);
+		glTexCoord2f(1,0);
+		glVertex3f(x2, y1, z2);
 	glEnd();
 }
 
-//Function to draw floor square
-void drawFloor(GLfloat x1, GLfloat x2, GLfloat z1, GLfloat z2)
-{
+void drawFloor(GLfloat x1, GLfloat x2, GLfloat z1, GLfloat z2) {
 	glBindTexture ( GL_TEXTURE_2D, g_ground);
 	glBegin(GL_POLYGON);
 		glNormal3f( 0.0, 1.0, 0.0);
-		//glColor4f(1, 1, 1, 1);
 		glColor3f(0.9f, 0.9f, 0.9f);
 
 		glTexCoord2f(0,0);
@@ -519,13 +431,12 @@ void drawFloor(GLfloat x1, GLfloat x2, GLfloat z1, GLfloat z2)
 		glVertex3f( x2, -1, z1 );
 		glTexCoord2f(0,1);
 		glVertex3f( x1, -1, z1 );
-    	glEnd();
+  glEnd();
 }
 
 //Trivial collision detection based on position of cubes in the map
 //Based on what is front, if close to the wall, return true
-bool checkCollision()
-{
+bool checkCollision() {
 	float camWorldX = (x+lx)/2;
 	float camWorldZ = (z+lz)/2;
 
@@ -548,8 +459,7 @@ bool checkCollision()
 }
 
 //Function to compute X and Z position
-void computePos(float deltaX, float deltaZ)
-{
+void computePos(float deltaX, float deltaZ) {
 	float oldx = x;
 	float oldz = z;
 
@@ -571,45 +481,16 @@ void computePos(float deltaX, float deltaZ)
 	}
 }
 
-void gameBeginScreen(){
-  
-  glBindTexture ( GL_TEXTURE_2D, g_start);
-	glBegin(GL_QUADS);
-		glVertex3f(5, -50, 5.0f);
-		glVertex3f(50, -50, 5.0f);
-		glVertex3f(50, 50, 5.0f);
-		glVertex3f(5, 50, 5.0f);
-	glEnd();
-	glColor4f(1, 1, 1, 1);
-		glPushMatrix();
-			glColor3f(1, 1, 1);
-			glTranslatef(10, 0, 10); 
-			glRasterPos2i(0, 0); 
-			draw_text("CS360 - PROJECT - OPENGL 3D");
-		glPopMatrix();
-		draw_text("Find the DIAMOND");
-  // draw_text("Click to play!");
-  // draw_text("Divya Chauhan 160246");
-  // draw_text("Rahul BS 160540");
-}
-
-
-void display(void)
-{
-	// cout<<"game status: "<<gameState<<"\n";
+void gameProgressScreen() {
 	//Check if goal is reached
-	
-	// cout<<x<<":"<<z<<","<<diamondx<<":"<<diamondz<<endl;
 	if (x > diamondx-1 && x < diamondx+1 && z > diamondz-1 && z < diamondz+1) {
 		cout<<x<<":"<<z<<endl;
 		gameState = GAME_WON;
 	}
-
 	//If there has been change in X and Z position, compute new position
 	if (deltaX || deltaZ) {
 		computePos(deltaX, deltaZ);
 	}
-
 	y = tilt+ly;
 
 	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
@@ -617,11 +498,6 @@ void display(void)
 	glClearColor(0.139, 0.134, 0.130, 1);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-
-	if(gameState == GAME_START){
-		gameBeginScreen();
-	}
-	else {
 
 	//Stop fps view when goal is reached
 	if (gameState == GAME_WON) {
@@ -644,8 +520,10 @@ void display(void)
 	//Camera vector
 	gluLookAt(x, tilt, z, // eye position
 			  x+lx, y, z+lz, // reference point
-			  0, 1, 0  // up vector
-		);
+			  0, 1, 0);  // up vector
+
+	cout<<"Eye Position: "<<x<<" "<<tilt<<" "<<z<<endl;
+	cout<<"Reference Point: "<<x+lx<<" "<<y<<" "<<z+lz<<"\n";
 
 	//Light for triangle blip
 	glDisable(GL_LIGHTING);
@@ -672,7 +550,6 @@ void display(void)
 	glPopMatrix();
 
 	//Changes to be made when goal is reached
-	
 	if (gameState == GAME_ON) {
 		//Lighting for text
 		glDisable(GL_LIGHTING);
@@ -682,15 +559,7 @@ void display(void)
 			set_light(light_1, -3, -4);
 		glPopMatrix();
 		glEnable(GL_LIGHTING);
-
-		// //Helper text
-		// glPushMatrix();
-		// 	glColor3f(1, 1, 1);
-		// 	glTranslatef(-2, 1, -1); // this will work
-		// 	glRasterPos2i(0, 0); // centre the text
-		// 	draw_text("Hi!, go right -->");
-		// glPopMatrix();
-	} 
+	}
 	else if(gameState == GAME_WON) {
 		//Light for text
 		glPushMatrix();
@@ -726,7 +595,6 @@ void display(void)
 
 	for(int i = 0; i < MAZE_SIZE; i++) {
 		for(int j=0; j < MAZE_SIZE; j++) {
-
 			//Stars at varying heights
 			if (i > 6 & j > 6) {
 				glPointSize(3.0f);
@@ -735,7 +603,6 @@ void display(void)
 				glPointSize (4.0f);
 				stary = 25;
 			}
-
 			//Draw points for stars
 			glPushMatrix ();
 				glBegin(GL_POINTS);
@@ -744,8 +611,9 @@ void display(void)
 				glEnd();
 			glPopMatrix();
 
-			//Draw a wall
 			if (maze[i][j] == 0) {
+				//Draw a wall
+				cout<<"Wall - (i,j): "<<i<<","<<j<<"\n";
 				glPushMatrix();
 					glTranslatef(2*i, 0, j*2);
 					glEnable(GL_TEXTURE_2D);
@@ -772,13 +640,66 @@ void display(void)
 			}
 		}
 	}
+}
+
+void gameBeginScreen(){
+	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+	glClearColor(0.139, 0.134, 0.130, 1);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	//Camera vector
+	gluLookAt(x, tilt, z, // eye position
+			  x+lx, y, z+lz, // reference point
+			  0, 1, 0);  // up vector
+
+	//Light for triangle blip
+	glDisable(GL_LIGHTING);
+	glPushMatrix();
+		glPointSize(5.0f);
+		set_light(light_1, x+lx, z+lz);
+	glPopMatrix();
+	glEnable(GL_LIGHTING);
+
+	int i=1, j=0;
+	GLfloat x1=1, x2=-1, y1=1, y2=-1, z=1;
+	glPushMatrix();
+		glTranslatef(2*i, 0, j*2);
+		glEnable(GL_TEXTURE_2D);
+		glBindTexture ( GL_TEXTURE_2D, g_start);
+		glBegin(GL_QUADS);
+			glColor4f(1, 1, 1, 1);
+			glNormal3f(0, 0, 1);
+			glTexCoord2f(1,1);
+			glVertex3f(x1, y1, z);
+		  glTexCoord2f(0,1);
+			glVertex3f(x2, y1, z);
+			glTexCoord2f(0,0);
+			glVertex3f(x2, y2, z);
+			glTexCoord2f(1,0);
+			glVertex3f(x1, y2, z);
+		glEnd();
+		glDisable(GL_TEXTURE_2D);
+	glPopMatrix();
+}
+
+void display(){
+	cout<<"gameState: "<<gameState<<"\n";
+	switch(gameState){
+		case GAME_START:
+			gameBeginScreen();
+			break;
+		case GAME_ON:
+			gameProgressScreen();
+			break;
+		default:
+			break;
 	}
-	glutSwapBuffers();
+	glFlush ();
+  glutSwapBuffers();
 }
 
 //Keyboard press handler
-void pressKey(unsigned char key, int xx, int yy)
-{
+void pressKey(unsigned char key, int xx, int yy) {
 	switch (key) {
 		//Change y to get a top down view and store current y and eye y
 		//to revert back to it when key is released
@@ -786,15 +707,21 @@ void pressKey(unsigned char key, int xx, int yy)
 			origTilt = tilt;
 			origLy = ly;
 			break;
-		case 'q' : 
+		case 'c':
+			cout<<"Continue the game\n";
+			gameState = GAME_ON;
+			glutPostRedisplay();
+			break;
+		case 'q' :
 			glutDestroyWindow(mainWindow);
 			exit(0);
 			break;
+		case 27:
+			exit(0);
 	}
 }
 
-void pressSpecialKey(int key, int xx, int yy)
-{
+void pressSpecialKey(int key, int xx, int yy) {
 	switch (key) {
 		case GLUT_KEY_UP : deltaX = 0.5f; break;
 		case GLUT_KEY_DOWN : deltaX = -0.5f; break;
@@ -804,17 +731,14 @@ void pressSpecialKey(int key, int xx, int yy)
 }
 
 //Keyboard press release handler
-void releaseKey(unsigned char key, int x, int y)
-{
+void releaseKey(unsigned char key, int x, int y) {
 	switch (key) {
-
 		case 'm': mapMode = false;
 			break;
 		}
 }
 
-void releaseSpecialKey(int key, int x, int y)
-{
+void releaseSpecialKey(int key, int x, int y) {
 	switch (key) {
 		case GLUT_KEY_UP : deltaX = 0.0f; break;
 		case GLUT_KEY_DOWN : deltaX = 0.0f; break;
@@ -823,14 +747,12 @@ void releaseSpecialKey(int key, int x, int y)
 	}
 }
 
-
 //Passive mouse movement callback
-void mouseMove(int xx, int yy)
-{
+void mouseMove(int xx, int yy) {
 	//turnAngle calculated for triangle blip rotation
 	float t = turnAngle;
 	turnAngle = -1 * (xx - xOrigin);
-	if (turnAngle>0){
+	if (turnAngle>0) {
 		turnAngle = turnAngle + 10;
 	} else if (turnAngle<0)
 		turnAngle = turnAngle - 10;
@@ -854,15 +776,11 @@ void mouseMove(int xx, int yy)
 		deltaAngle = dAngle;
 		deltaAngleY = dAngleY;
 	}
-
-
-	//
 	glutSetWindow(mainWindow);
 }
 
 //Loading textures
-void load_and_bind_textures()
-{
+void load_and_bind_textures() {
 	g_wall = load_and_bind_texture("./wall.png");
 	g_ground = load_and_bind_texture("./tile.png");
 	g_start = load_and_bind_texture("./start.png");
@@ -878,7 +796,6 @@ void init() {
 }
 
 int main(int argc, char **argv) {
-
 	// init GLUT and create window
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
@@ -891,15 +808,14 @@ int main(int argc, char **argv) {
 	glutReshapeFunc(reshape);
 	glutIdleFunc(display);
 
-
 	//Keyboard callbacks
 	glutKeyboardFunc(pressKey);
 	glutIgnoreKeyRepeat(1);
 	glutKeyboardUpFunc(releaseKey);
 
 	//Special Key callbacks
-	glutSpecialFunc(pressSpecialKey); 	
-	glutSpecialUpFunc(releaseSpecialKey); 
+	glutSpecialFunc(pressSpecialKey);
+	glutSpecialUpFunc(releaseSpecialKey);
 
 	//Mouse Callbacks
 	glutPassiveMotionFunc(mouseMove);
@@ -917,7 +833,5 @@ int main(int argc, char **argv) {
 	createGLUTMenus ();
 	// enter GLUT event processing cycle
 	glutMainLoop();
-
-	return 1;
+	return 0;
 }
-
